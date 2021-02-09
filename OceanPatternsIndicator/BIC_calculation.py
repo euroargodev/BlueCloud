93efd6b484f6c7a1b6c7a723c4497f5f0fc6796d
loicbachelot <loic.bachelot@gmail.com>
2021-02-09 18:00:43 +0100

    [update] made bic_min argument in plot_BIC optional

--- OceanPatternsIndicator/BIC_calculation.py
+++ OceanPatternsIndicator/BIC_calculation.py
@@ -34,36 +34,36 @@ def mapping_corr_dist(corr_dist, start_point, grid_extent):
                '''
 
     # angular distance d/earth's radius (km)
-    delta = corr_dist/6371
+    delta = corr_dist / 6371
 
     # all in radians (conversion at the end)
-    grid_extent = grid_extent*np.pi/180
-    start_point = start_point*np.pi/180
+    grid_extent = grid_extent * np.pi / 180
+    start_point = start_point * np.pi / 180
 
     ### while loop for lat nord ###
     max_lat = grid_extent[3]
-    lat2 = -np.pi/2
+    lat2 = -np.pi / 2
     lat1 = start_point[1]
     # bearing = 0 donc cos(0)=1 and sin(0)=0
     new_lats = [lat1]
     while lat2 < max_lat:
-        lat2 = np.arcsin(np.sin(lat1)*np.cos(delta) +
-                         np.cos(lat1)*np.sin(delta))
+        lat2 = np.arcsin(np.sin(lat1) * np.cos(delta) +
+                         np.cos(lat1) * np.sin(delta))
         new_lats.append(lat2)
         lat1 = lat2
 
     ### while loop for lat sud ###
     min_lat = grid_extent[2]
-    lat2 = np.pi/2
+    lat2 = np.pi / 2
     lat1 = start_point[1]
     # bearing = pi donc cos(pi)=-1 and sin(pi)=0
     while lat2 > min_lat:
-        lat2 = np.arcsin(np.sin(lat1)*np.cos(delta) -
-                         np.cos(lat1)*np.sin(delta))
+        lat2 = np.arcsin(np.sin(lat1) * np.cos(delta) -
+                         np.cos(lat1) * np.sin(delta))
         new_lats.append(lat2)
         lat1 = lat2
 
-    new_lats = np.sort(new_lats)*180/np.pi
+    new_lats = np.sort(new_lats) * 180 / np.pi
 
     ### while loop for lon east ###
     max_lon = grid_extent[1]
@@ -72,8 +72,8 @@ def mapping_corr_dist(corr_dist, start_point, grid_extent):
     lat1 = start_point[1]
     # bearing = pi/2 donc cos(pi/2)=0 and sin(pi/2)=1
     new_lons = [lon1]
-    dlon = np.arctan2(np.sin(delta)*np.cos(lat1),
-                      np.cos(delta)-np.sin(lat1)*np.sin(lat1))
+    dlon = np.arctan2(np.sin(delta) * np.cos(lat1),
+                      np.cos(delta) - np.sin(lat1) * np.sin(lat1))
     while lon2 < max_lon:
         lon2 = lon1 + dlon
         new_lons.append(lon2)
@@ -85,14 +85,14 @@ def mapping_corr_dist(corr_dist, start_point, grid_extent):
     lon1 = start_point[0]
     lat1 = start_point[1]
     # bearing = -pi/2 donc cos(-pi/2)=0 and sin(-pi/2)=-1
-    dlon = np.arctan2(-np.sin(delta)*np.cos(lat1),
-                      np.cos(delta)-np.sin(lat1)*np.sin(lat1))
+    dlon = np.arctan2(-np.sin(delta) * np.cos(lat1),
+                      np.cos(delta) - np.sin(lat1) * np.sin(lat1))
     while lon2 > min_lon:
         lon2 = lon1 + dlon
         new_lons.append(lon2)
         lon1 = lon2
 
-    new_lons = np.sort(new_lons)*180/np.pi
+    new_lons = np.sort(new_lons) * 180 / np.pi
 
     return new_lats, new_lons
 
@@ -123,7 +123,7 @@ def mapping_corr_time(corr_time, start_point, time_extent):
     # bearing = 0 donc cos(0)=1 and sin(0)=0
     new_time = [time1]
     while time2 < max_time:
-        time2 = time1 + np.timedelta64(corr_time*30, 'D')
+        time2 = time1 + np.timedelta64(corr_time * 30, 'D')
         new_time.append(time2)
         time1 = time2
 
@@ -133,7 +133,7 @@ def mapping_corr_time(corr_time, start_point, time_extent):
     time1 = start_point[0]
     # bearing = pi donc cos(pi)=-1 and sin(pi)=0
     while time2 > min_time:
-        time2 = time1 - np.timedelta64(corr_time*30, 'D')
+        time2 = time1 - np.timedelta64(corr_time * 30, 'D')
         new_time.append(time2)
         time1 = time2
 
@@ -143,42 +143,42 @@ def mapping_corr_time(corr_time, start_point, time_extent):
 
 
 def BIC_cal(X, k, pcm_features):
-        ''' Function that calculates BIC for a number of classes k
+    ''' Function that calculates BIC for a number of classes k
 
-                Parameters
-                ----------
-                    X: dataset after preprocessing
-                    k: number of classes
+            Parameters
+            ----------
+                X: dataset after preprocessing
+                k: number of classes
 
-                Returns
-                ------
-                    BIC: BIC value
-                    k: number of classes
+            Returns
+            ------
+                BIC: BIC value
+                k: number of classes
 
-               '''
+           '''
 
-        # create model
-        m = pcm(K=k+1, features=pcm_features)
-        # fit model
-        m._classifier.fit(X)
+    # create model
+    m = pcm(K=k + 1, features=pcm_features)
+    # fit model
+    m._classifier.fit(X)
 
-        # calculate LOG LIKEHOOD
-        llh = m._classifier.score(X)
+    # calculate LOG LIKEHOOD
+    llh = m._classifier.score(X)
 
-        # calculate Nb of independant parameters to estimate
-        # we suppose m._classifier.covariance_type == 'full'
-        _, n_features = m._classifier.means_.shape
-        cov_params = m._classifier.n_components * \
-            n_features * (n_features + 1) / 2.
-        mean_params = n_features * m._classifier.n_components
-        Nf = int(cov_params + mean_params + m._classifier.n_components - 1)
+    # calculate Nb of independant parameters to estimate
+    # we suppose m._classifier.covariance_type == 'full'
+    _, n_features = m._classifier.means_.shape
+    cov_params = m._classifier.n_components * \
+                 n_features * (n_features + 1) / 2.
+    mean_params = n_features * m._classifier.n_components
+    Nf = int(cov_params + mean_params + m._classifier.n_components - 1)
 
-        # calculate bic
-        N_samples = X.shape[0]
-        BIC = (-2 * llh * N_samples + Nf * np.log(N_samples))
-        #BIC = m._classifier.bic(X)
+    # calculate bic
+    N_samples = X.shape[0]
+    BIC = (-2 * llh * N_samples + Nf * np.log(N_samples))
+    # BIC = m._classifier.bic(X)
 
-        return BIC, k
+    return BIC, k
 
 
 def BIC_calculation(ds, corr_dist, coords_dict, time_steps, pcm_features, features_in_ds, z_dim, Nrun=10, NK=20):
@@ -219,7 +219,7 @@ def BIC_calculation(ds, corr_dist, coords_dict, time_steps, pcm_features, featur
         d1 = datetime.strptime(time_steps[0], "%Y-%m")
         d2 = datetime.strptime(time_steps[1], "%Y-%m")
         num_months = abs(d1.year - d2.year) * 12 + abs(d1.month - d2.month)
-        #print(num_months)
+        # print(num_months)
         if num_months < 6:
             warnings.warn(
                 "Chosen time steps are too near, you may not obtain a minimum in the BIC function. If this is the case, please try with more distant time steps")
@@ -240,9 +240,9 @@ def BIC_calculation(ds, corr_dist, coords_dict, time_steps, pcm_features, featur
             new_lats, new_lons = mapping_corr_dist(
                 corr_dist=corr_dist, start_point=np.concatenate((lonp, latp)), grid_extent=grid_extent)
 
-            ds.sel({'latitude':30, 'longitude':30 }, method='nearest')
-            ds_run_i = ds.sel({'latitude':list(new_lats), 'longitude':list(new_lons)}, method='nearest')
-            ds_run_i = ds_run_i.sel({'time':time_steps[itime]})
+            ds.sel({'latitude': 30, 'longitude': 30}, method='nearest')
+            ds_run_i = ds.sel({'latitude': list(new_lats), 'longitude': list(new_lons)}, method='nearest')
+            ds_run_i = ds_run_i.sel({'time': time_steps[itime]})
 
             # change lat and lot dimensions by index to be able to merge the datasets (it is not necessary to have lat lon information)
             n_lati = ds_run_i[coords_dict.get('latitude')].size
@@ -274,17 +274,17 @@ def BIC_calculation(ds, corr_dist, coords_dict, time_steps, pcm_features, featur
         results = []
         for k in class_list:
             results.append(BIC_cal(X, k, pcm_features))
-            
+
         results = [r for r in results if r is not None]
         results.sort(key=lambda x: x[1])
         BIC[:, run] = np.array([i[0] for i in results])
 
-    BIC_min = np.argmin(np.mean(BIC, axis=1))+1
+    BIC_min = np.argmin(np.mean(BIC, axis=1)) + 1
 
     return BIC, BIC_min
 
 
-def plot_BIC(BIC, NK, bic_min):
+def plot_BIC(BIC, NK, bic_min=-1):
     '''Plot of mean BIC (Bayesian Information Criteria) and standard deviation.
 
            Parameters
@@ -300,20 +300,21 @@ def plot_BIC(BIC, NK, bic_min):
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 5), dpi=90)
     BICmean = np.mean(BIC, axis=1)
     BICstd = np.std(BIC, axis=1)
-    normBICmean = (BICmean-np.mean(BICmean))/np.std(BICmean)
-    #normBICstd = np.std(normBICmean)
-    #plt.plot(np.arange(kmax)+1,(BIC-np.mean(BIC))/np.std(BIC),label='Raw BIC')
-    plt.plot(np.arange(NK)+1, BICmean, label='BIC mean')
-    plt.plot(np.arange(NK)+1, BICmean+BICstd,
-             color=[0.7]*3, linewidth=0.5, label='BIC std')
-    plt.plot(np.arange(NK)+1, BICmean-BICstd, color=[0.7]*3, linewidth=0.5)
+    normBICmean = (BICmean - np.mean(BICmean)) / np.std(BICmean)
+    # normBICstd = np.std(normBICmean)
+    # plt.plot(np.arange(kmax)+1,(BIC-np.mean(BIC))/np.std(BIC),label='Raw BIC')
+    plt.plot(np.arange(NK) + 1, BICmean, label='BIC mean')
+    plt.plot(np.arange(NK) + 1, BICmean + BICstd,
+             color=[0.7] * 3, linewidth=0.5, label='BIC std')
+    plt.plot(np.arange(NK) + 1, BICmean - BICstd, color=[0.7] * 3, linewidth=0.5)
     plt.ylabel('BIC')
     plt.xlabel('Number of classes')
-    plt.xticks(np.arange(NK)+1)
+    plt.xticks(np.arange(NK) + 1)
     plt.legend()
     plt.title('Bayesian information criteria (BIC)')
-    ax.annotate('min = ' + str(bic_min), xy=(bic_min, BICmean[bic_min - 1]), xycoords='data',
-                xytext=(0.8, 0.8), textcoords='axes fraction',
-                arrowprops=dict(facecolor='black', shrink=0.05, width=3),
-                horizontalalignment='right', verticalalignment='top',
-                )
+    if bic_min > -1:
+        ax.annotate('min = ' + str(bic_min), xy=(bic_min, BICmean[bic_min - 1]), xycoords='data',
+                    xytext=(0.8, 0.8), textcoords='axes fraction',
+                    arrowprops=dict(facecolor='black', shrink=0.05, width=3),
+                    horizontalalignment='right', verticalalignment='top',
+                    )
