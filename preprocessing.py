7bb0b25e1eb16bb156da937389dffa17d55acd7d
andreagjuan91@gmail.com <agarciaj_local@semoy.home>
2021-02-26 15:17:54 +0100

    [add] add functions weekly_mean, hist_2D, reduce_dims, scaler and apply_PCA

--- preprocessing.py
+++ preprocessing.py
@@ -3,8 +3,9 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
 
-def weekly_mean():
+def weekly_mean(ds):
     '''Weekly mean in dataset
 
            Parameters
@@ -22,13 +23,54 @@ def weekly_mean():
                '''
 
     #TODO: when more than one year in dataset
-    #TODO: option 2D plot
-    #TODO: change 'week' to feature
+    #TODO: detect time variable
+    #TODO: detect var_name
     
-    return new_lats, new_lons
+    var_name = 'CHL'
+    X = ds.groupby("time.week").mean()
+    X = X.rename_dims({'week': 'feature'})
+    X = X.rename({'week': 'feature'})
+    
+    return X
+    
+def hist_2D(X, bins=None):
+    '''Plot 2D histogram
+
+           Parameters
+           ----------
+               corr_dist: correlation distance
+               start_point: latitude and longitude of the start point 
+               grid_extent: max and min latitude and longitude of the grid to be remapped 
+                    [min lon, max lon, min let, max lat]
+
+           Returns
+           ------
+               new_lats: new latitude vector with points separeted the correlation distance 
+               new_lons: new longitude vector with points separeted the correlation distance
+
+               '''
+    #TODO: detect var_name
+    var_name = 'CHL'
+    
+    if np.any(bins) == None: 
+        bins = np.linspace(X[var_name].min().values, X[var_name].max().values,num=50)
+
+    histo_2d = [] 
+    for iweek in range(np.size(X.feature)):
+        hist_values, bin_edges = np.histogram(X[var_name].isel(feature=iweek).values, bins=bins)
+        histo_2d.append(hist_values)
+    
+    fig, ax = plt.subplots(figsize=(12,10))
+
+    plt.pcolormesh(X.feature.values, bins, np.transpose(histo_2d), cmap='Reds', edgecolors='black')
+    cbar = plt.colorbar()
+    ax.set_ylabel(var_name)
+    ax.set_xlabel('Weeks')
+    cbar.ax.set_ylabel('Counts')
 
 
-def reduce_dims():
+
+def reduce_dims(X):
     '''Reduce lat lon dimensions to sampling dimension 
 
            Parameters
@@ -45,8 +87,11 @@ def reduce_dims():
                '''
 
     #TODO: dimensions detection
+    sampling_dims = list(X.dims)
+    sampling_dims.remove('feature')
+    X = X.stack({'sampling': sampling_dims})
     
-    return new_time
+    return X
 
 def delate_NaNs():
     ''' Delate NaNs in dataset
@@ -69,7 +114,7 @@ def delate_NaNs():
 
     return BIC, k
 
-def scaler():
+def scaler(X):
     ''' Scale data
 
             Parameters
@@ -85,10 +130,20 @@ def scaler():
             '''
 
     # TODO: option more than one scaler
+    #TODO: detect var_name
+    var_name = 'CHL'
+    
+    # Check dimensions order
+    X = X.transpose("sampling", "feature")
+    
+    # Apply Standard Scaler
+    from sklearn.preprocessing import StandardScaler
+    X_scale = StandardScaler().fit_transform(X[var_name])
+    X = X.assign(variables={var_name + "_scaled":(('sampling', 'feature'), X_scale)})
 
-    return BIC, k
+    return X
 
-def PCA():
+def apply_PCA(X, plot_var=False):
     ''' Principal components analysis
 
             Parameters
@@ -103,10 +158,26 @@ def PCA():
 
             '''
 
-    # TODO: at each time be sur that the dataset is in the right order
-    # TODO: option plot
+    # TODO: detect var_name
+    var_name = 'CHL'
+    
+    # Check dimensions order
+    X = X.transpose("sampling", "feature")
+    
+    from sklearn.decomposition import PCA
+    pca = PCA(n_components = 0.99, svd_solver = 'full')
+    pca = pca.fit(X[var_name + "_scaled"])
+    X_reduced = pca.transform(X[var_name + "_scaled"])
+    X = X.assign(variables={var_name + "_reduced":(('sampling', 'feature_reduced'),X_reduced)})
+    
+    if plot_var:
+        fig, ax = plt.subplots()
+        pb = plt.bar(range(pca.n_components_), pca.explained_variance_ratio_)
+        ax.set_xlabel('n_components')
+        ax.set_ylabel('Percentage')
+        ax.set_title('Percentage of variance explained by each of the selected components')
 
-    return BIC, k
+    return X
 
 def unstack_dataset():
     ''' UNstack dataste
